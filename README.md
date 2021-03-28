bbc838c9977cfccb5712536b6382567a598aaf9c
tommyli123@gmail.com <tommy123@gmail.com>
2021-03-27 20:28:59 -0700

    init

---
+++ README.md
@@ -0,0 +1,48 @@
+# Zen
+This is a collection of webbots that crawl sites to look for COVID vaccine appointments.
+Only Sutter Health is supported currently, but it can be extended easily to support other sites.
+
+# Installation
+* Install python3 in your system.  
+  * For MacOS, follow this link https://docs.python-guide.org/starting/install3/osx/
+* create virtual environment in the cloned directory 
+```
+$ cd /path/to/<clonedDirectory>
+$ python3 -m venv venv
+```
+* start the virtual environment
+```
+$ source venv/bin/activate
+```
+* install the required packages
+```
+$ pip3 install -r requirements.txt
+```
+* download ChromeDriver with the version that matches your Chrome browser version
+  * follow this link https://chromedriver.chromium.org/downloads
+* prepare config.json
+```
+$ cp config.json.template config.json
+```
+* update your credential and the chromeDriver path in config.json
+  * sutter health is the only supported site for now
+```json
+{
+    "sutter": {
+        "userid": "yourvalue",
+        "password": "yourvalue"
+    },
+    "chromeDriverPath": "yourpathtochromedriver",
+    "delay": 2
+}
+
+```
+
+# Run
+Execute the corresponding health provider site webbot script, it will show the appointments and pause for the delay time * 10.   During the pause time, 
+1. if the page shows appointments that you like, press CTRL-C in the terminal that runs the script to terminate and directly make the appoinment in the browser.
+2. if no appointment is shown or the appointments shown is not what you like, you can let the script to terminate the browser after the pause time expires and the whole process will start over again  infinitely.
+3. You can terminate the infinite loop in 2) by CTRL-C in the terminal running the script.  Remember to close the browser opened by the script. 
+```
+$ python sutter.py
+```
---
+++ sutter.py
@@ -0,0 +1,61 @@
+import time, json
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+config = json.loads(open('config.json').read())
+delay = config['delay']
+while True:
+    driver = webdriver.Chrome(config['chromeDriverPath'])  # Optional argument, if not specified will search path.
+    # driver.set_window_size(1400, 800)
+    driver.maximize_window()
+    driver.get('https://mho.sutterhealth.org/');
+    time.sleep(1) # Let the user actually see something!
+    online_id = driver.find_element_by_id('online-id-form')
+    online_id.send_keys(config['sutter']['userid'])
+    password = driver.find_element_by_id('password-form')
+    password.send_keys(config['sutter']['password'])
+    signin_btn = driver.find_element_by_class_name('cta--form-submit.teal.js-dashboard-sign-in')
+    signin_btn.click()
+
+    time.sleep(delay)
+    driver.find_element_by_xpath('//*[@id="links"]/a[2]').click()
+
+    time.sleep(delay)
+    driver.find_element_by_xpath('//*[@id="LQL_svU0KbYdMl9OVZBvT+uFDA==_1"]').click()
+    driver.find_element_by_xpath('//*[@id="LQL_EExHV2uxeb5PW7nmL1JnGg==_5"]').click()
+    driver.find_element_by_xpath('//*[@id="next-step"]').click() # proceed to next page
+
+    time.sleep(delay)
+    # driver.find_element_by_xpath('//*[@id="LQL_g9n4TlB7VSMvVVmuCi5r/w==_10"]').click()
+    driver.find_element_by_xpath('//*[@id="LQL_g9n4TlB7VSMvVVmuCi5r/w==_1"]').click()
+    driver.find_element_by_xpath('//*[@id="LQL_0D02j4T9UiZWAcD6abv9YQ==_1"]').click()
+    driver.find_element_by_xpath('//*[@id="next-step"]').click() # proceed to next page
+
+    # skip following due to previous page choices
+    #time.sleep(delay)
+    #driver.find_element_by_xpath('//*[@id="LQL_uxCOQHNIY+IXYANPoxHhJw==_1"]').click()
+    #driver.find_element_by_xpath('//*[@id="questionform"]/div[2]/input').click() # proceed to next page
+
+    time.sleep(delay)
+    driver.find_element_by_xpath('//*[@id="LQL_RgEm6XD4rMEAB2wDsVz8xA==_1"]').click()
+    driver.find_element_by_xpath('//*[@id="LQL_J6kVWdN36hEAidaOkPL2Ig==_1"]').click()
+    driver.find_element_by_xpath('//*[@id="LQL_PSXl9st+MlbPZMBG1tZNDQ==_1"]').click()
+    driver.find_element_by_xpath('//*[@id="LQL_rK/N8sMtygk/NV80AYYURw==_1"]').click()
+    driver.find_element_by_xpath('//*[@id="LQL_x77HwN5T4VHjd2TBcEwIgw==_1"]').click()
+    driver.find_element_by_xpath('//*[@id="LQL_ReDtqQRDiSbT6KOrItAOMw==_1"]').click()
+    driver.find_element_by_xpath('//*[@id="LQL_2rfUQXcgyTiA05NnsuYWzg==_1"]').click()
+    driver.find_element_by_xpath('//*[@id="next-step"]').click()
+
+    time.sleep(delay)
+    driver.find_element_by_xpath('//*[@id="LQL_vulxIKNBu33Jr/Z/0utMAQ==_1"]').click()
+    driver.find_element_by_xpath('//*[@id="next-step"]').click()
+
+    time.sleep(delay)
+    driver.find_element_by_xpath('//*[@id="scheduling-workflow"]/div[7]/div[1]/div/div[2]/div/a[1]').click()
+    driver.find_element_by_xpath('//*[@id="scheduling-continue"]').click()
+
+    # it should now show the appointment availability page
+    time.sleep(delay * 10) # Change the factor here to allow enough time for appointments to load
+    driver.quit()
