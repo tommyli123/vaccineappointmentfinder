0ffc575b094363c907a7736f14f879fe2f98ec67
tommyli123 <tommyli123@gmail.com>
2021-04-19 13:34:20 -0700

    add sms notification

--- washington.py
+++ washington.py
@@ -1,4 +1,5 @@
 import time, json, re
+import smtplib
 from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -11,9 +12,18 @@ config = json.loads(open('config.json').read())
 delay = config['delay']
 zipcode = str(config['washington']['zipcode'])
 distance = config['washington']['distance']
+sms_enabled = config['sms']['enabled']
+if sms_enabled:
+    sms_number = config['sms']['number']
+gmail_userid = config['gmail']['userid']
+gmail_password = config['gmail']['password']
 
 options = Options()
 options.add_argument('--headless')
+server = smtplib.SMTP( "smtp.gmail.com", 587 )
+server.starttls()
+server.login(gmail_userid, gmail_password)
+
 while True:
     driver = webdriver.Chrome(config['chromeDriverPath'], options=options)  # Optional argument, if not specified will search path.
     # driver.set_window_size(1400, 800)
@@ -24,28 +34,39 @@ while True:
     sites = driver.find_elements_by_class_name('justify-between')
     m = re.compile('^Available Appointments: ([0-9]+)$')
     total_available = 0
+    msg = []
     for eachSite in sites:
         p = eachSite.find_elements_by_tag_name('p')
         if len(p) == 0:
             continue
         _t = p[8].text
         g = m.match(_t)
-        available_appointments = int(g[1])
+        try:
+            available_appointments = int(g[1])
+        except:
+            print(f"ERROR : cannot parse {_t}")
         total_available = total_available + available_appointments
         if available_appointments > 0:
-            print(f"***** {datetime.now()} *****")
-            print(p[0].text)
-            print(p[1].text)
-            print(p[7].text)
-            print(_t)
-            print("=======================")
+            _m = []
+            _m.append(f"***** {datetime.now()} *****")
+            _m.append(p[0].text)
+            _m.append(p[1].text)
+            _m.append(p[7].text)
+            _m.append(_t)
+            _m.append("=======================")
+            print('\n'.join(_m))
+            msg = msg + _m
     if total_available == 0:
         print(f"***** {datetime.now()} *****")
         print(f"No appointments available. Retry in {delay} seconds")
         print("=======================")
         time.sleep(delay)
     else:
+        msg.append(url)
+        if sms_enabled: 
+            server.sendmail( 'VaccineFinder', sms_number, '\n'.join(msg) )
         print(url)
+        print('text message sent!')
         print("=======================")
 
     driver.quit()
